1b03ade3a16739c8aa350fecafa3787d4c008dff
alamandaru <alamandaru@student.telkomuniversity.ac.id>
2022-12-13 10:19:23 +0700

    debg

--- MODUL5/app/Models/Showrooms.php
+++ MODUL5/app/Models/Showrooms.php
@@ -8,5 +8,5 @@ use Illuminate\Database\Eloquent\Model;
 class Showrooms extends Model
 {
     use HasFactory;
-    protected $guarded = ['id'];
+    $guarded = ['id'];
 }
--- MODUL5/routes/web.php
+++ MODUL5/routes/web.php
@@ -1,21 +1,20 @@
-<?php
+            <?php
 
-use App\Http\Controllers\ShowroomsController;
-use Illuminate\Support\Facades\Route;
+            use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+            /*
+            |--------------------------------------------------------------------------
+            | Web Routes
+            |--------------------------------------------------------------------------
+            |
+            | Here is where you can register web routes for your application. These
+            | routes are loaded by the RouteServiceProvider within a group which
+            | contains the "web" middleware group. Now create something great!
+            |
+            */
 
-Route::get('/', function () {
-    return view('home');
-});
+            Route::post('/home', function () {
+                return view('home');
+            });
 
-route::resource("/mobil",ShowroomsController::class);
+            route::resource("/mobil",ShowroomsController::class);
